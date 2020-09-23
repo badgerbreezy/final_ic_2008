2bba718507c1fc6ec64b992f2f8cd1a8c4df8d2c
Brian Liu <brian.b.liu@gmail.com>
2020-09-23 09:33:54 -0600

    Add test it can check ingredients.

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -27,4 +27,16 @@ class RecipeTest < Minitest::Test
 
     assert_equal ({ingredient1 => 6, ingredient2 => 8}), recipe1.ingredients_required
   end
+
+  def test_it_can_check_ingredients
+    recipe1 = Recipe.new("Mac and Cheese")
+    ingredient1 = Ingredient.new({name: "Cheese",   unit: "oz", calories: 50})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+
+    recipe1.add_ingredient(ingredient1, 2)
+    recipe1.add_ingredient(ingredient1, 4)
+    recipe1.add_ingredient(ingredient2, 8)
+
+    assert_equal [ingredient1, ingredient2], recipe1.ingredients
+  end
 end

4c7a512dfa532fc12a53aa256f3d0d3e566f948b
Brian Liu <brian.b.liu@gmail.com>
2020-09-23 10:04:34 -0600

    Add method ingredients to cookbook.

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -7,4 +7,14 @@ class CookBook
   def add_recipe(recipe)
     @recipes << recipe
   end
+
+  def ingredients
+    ingredient_list = []
+    @recipes.each do |recipe|
+      recipe.ingredients_required.each do |ingredient, amount|
+        ingredient_list << ingredient.name
+      end
+    end
+    ingredient_list.uniq
+  end
 end

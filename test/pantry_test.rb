3968c8a360691bffa6d6fecf1df432ef2efa30bd
Brian Liu <brian.b.liu@gmail.com>
2020-09-23 09:14:38 -0600

    Revise pantry test it exists and has attributes.

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -7,11 +7,15 @@ require './lib/pantry'
 class PantryTest < Minitest::Test
   def test_it_exists_and_has_attributes
     pantry = Pantry.new
+    ingredient1 = Ingredient.new({name: "Cheese",   unit: "oz", calories: 50})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
 
+    assert_instance_of Pantry, pantry
     assert_instance_of Ingredient, ingredient1
     assert_equal "Cheese", ingredient1.name
     assert_equal "oz", ingredient1.unit
     assert_equal 50, ingredient1.calories
+    assert_equal ({}), pantry.stock
   end
 
 end
